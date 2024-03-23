c7c40a4d69563a31846dd3b888f30e8047816b18
xxsecondxx1 <second_u2@hotmail.com>
2024-03-22 19:16:32 -0500

    Se agrega el proyecto

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace prueba_github.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
